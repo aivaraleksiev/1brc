5dbcb46edf7c288add688fd758f4e70aa127f4d0
Aivar Aleksiev <aivar.aleksiev@gmail.com>
2025-06-25 23:44:47 +0300

    Update Utils.h

--- 1brc src/Utils.h
+++ 1brc src/Utils.h
@@ -2,8 +2,15 @@
 
 #if defined(_MSC_VER)
    #define ALWAYS_INLINE __forceinline
+   #define BEGIN_HOT_SEGMENT __pragma(code_seg(push, hot_seg, ".text$hot"))
+   #define END_HOT_SEGMENT   __pragma(code_seg(pop, hot_seg))
 #elif defined(__GNUC__) || defined(__clang__)
-   #define ALWAYS_INLINE __attribute__((always_inline)) inline
+   #define ALWAYS_INLINE __attribute__((always_inline, hot)) inline
+   #define BEGIN_HOT_SEGMENT
+   #define END_HOT_SEGMENT
 #else
-   #define ALWAYS_INLINE inline // fallback
+   // fallback
+   #define ALWAYS_INLINE inline
+   #define BEGIN_HOT_SEGMENT
+   #define END_HOT_SEGMENT
 #endif
